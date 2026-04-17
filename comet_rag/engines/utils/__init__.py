9ed86412cc36f24107afd6825b2be4c8074ad4ce
comet-7x <1933472218>
2026-04-17 15:30:18 +0800

    feat: 新增`Magika` 库用于识别文件后缀

--- comet_rag/engines/utils/__init__.py
+++ comet_rag/engines/utils/__init__.py
@@ -1,4 +1,9 @@
 from .base64 import from_base64_url, guess_mime_type, image_to_base64, to_base64_url
+from .file_detector import (
+    detect_content_type_from_bytes,
+    detect_content_type_from_path,
+    detect_content_type_from_stream,
+)
 from .misc import compute_sha256
 
 __all__ = [
@@ -7,4 +12,7 @@ __all__ = [
     "to_base64_url",
     "guess_mime_type",
     "image_to_base64",
+    "detect_content_type_from_bytes",
+    "detect_content_type_from_path",
+    "detect_content_type_from_stream",
 ]
